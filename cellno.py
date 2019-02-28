94111086a5331cee5cdbfd33b0eaae0d716e78aa
Ntsikelelo Maze <33375312+ntsikelelosonjica@users.noreply.github.com>
2019-02-28 11:03:46 +0200

    Update cellno.py
    Needed to do small changes.

--- cellno.py
+++ cellno.py
@@ -1,12 +1,12 @@
 '''
-author: Maze
+author: Ntsikelelo Sonjica
 module: cellno.py
 
 Here I am making use of OOP.The program
 generates a random list of South African
 Cell Phone numbers using the following
 prefixes '073', '074' and '079'. It then
-outputs the first generated cellphone numb
+outputs the first generated cellphone number
 '''
 
 from random import randint;
@@ -14,7 +14,7 @@ from random import randint;
 class CellNo(object):
         def cell_no(self):
                 prefix = ["073","074","079"];
-                suffix = [str(randint(1000000,9999999))];
+                suffix = [str(randint(1000000,9999999)) for i in range(len(prefix))];
                 cell = [];
                 for p in prefix:
                         for s in suffix:
